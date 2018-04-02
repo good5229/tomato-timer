36ec12983f0ffeca4c60a96ae544b54066b8e526
good5229 <good5229@naver.com>
2018-04-02 16:10:47 +0900

    feat(redux):finish

--- components/Timer/presenter.js
+++ components/Timer/presenter.js
@@ -2,7 +2,38 @@ import React, { Component } from "react";
 import { View, StyleSheet, Text, StatusBar } from "react-native";
 import Button from "../Button";
 
+function formatTime(time) {
+    var minutes = Math.floor(time / 60);
+    time -= minutes * 60;
+  
+    var seconds = parseInt(time % 60, 10);
+  
+    return `${minutes < 10 ? `0${minutes}` : minutes}:${seconds < 10
+      ? `0${seconds}`
+      : seconds}`;
+  
+    return;
+  }
+
 class Timer extends Component {
+
+    componentWillReceiveProps(nextProps){
+        const currentProps = this.props;
+        if(currentProps.isPlaying === false && nextProps.isPlaying === true){
+            const timerInterval = setInterval(()=>{
+                currentProps.addSecond();
+            }, 1000);
+            this.setState({
+                timerInterval
+            })
+        } else if(currentProps.isPlaying === true && nextProps.isPlaying === false){
+            clearInterval(this.state.timerInterval);
+        }
+        console.log(
+            'the current props are: ${currentProps.isPlaying} and the new ones are ${nextProps.isPlaying}'
+        );
+    }
+
   render() {
     const { isPlaying, elapsedTime, timerDuration, startTimer, restartTimer, addSecond } = this.props;
     return (
